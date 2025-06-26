bf3c316b3e6e9dc07012cce88fb2556f61f14b00
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-26 15:53:00 +0530

    added a userCard

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -3,6 +3,7 @@ import { BASE_URL } from "../utils/constants";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 function Feed() {
   const dispatch = useDispatch();
@@ -21,7 +22,13 @@ function Feed() {
   useEffect(() => {
     getFeed();
   }, []);
-  return <div>Feed</div>;
+  return (
+    feed && (
+      <div className="flex justify-center my-auto">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
 }
 
 export default Feed;
---
+++ src/Components/UserCard.jsx
@@ -0,0 +1,31 @@
+function UserCard({ user }) {
+  if (!user) return;
+  const { firstName, lastName, about, age, gender } = user;
+  //   console.log(user);
+  return (
+    <div>
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {firstName} {lastName}
+          </h2>
+          <a>{age}</a>
+          <a>{gender}</a>
+          <p>{about}</p>
+          <div className="card-actions justify-center my-4">
+            <button className="btn btn-secondary">Interested</button>
+            <button className="btn btn-primary">Ignore</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserCard;
