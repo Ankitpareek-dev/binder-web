d38a6fe0639e65be8c08419b1d8800232404f017
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-26 12:42:55 +0530

    added a logout logic

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -3,14 +3,16 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { BASE_URL } from "../utils/constants";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useEffect } from "react";
 
 function Body() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const userData = useSelector((store) => store.user);
   const fetchUser = async () => {
+    if (userData) return;
     try {
       const user = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
@@ -25,7 +27,9 @@ function Body() {
   };
 
   useEffect(() => {
-    fetchUser();
+    if (!userData) {
+      fetchUser();
+    }
   }, []);
   return (
     <>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,34 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { removeUser } from "../utils/userSlice";
 
 function NavBar() {
   const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleLogout = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/logout",
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+    }
+  };
   if (!user) return null;
   // console.log(user.photoId);
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Binder💓</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          Binder💓
+        </Link>
       </div>
       <div className="flex gap-2">
         <div>Hello {user.firstName}</div>
@@ -28,16 +49,16 @@ function NavBar() {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link to="/profile" className="justify-between">
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li>
               <a>Settings</a>
             </li>
             <li>
-              <a>Logout</a>
+              <a onClick={() => handleLogout()}>Logout</a>
             </li>
           </ul>
         </div>
